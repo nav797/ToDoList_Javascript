d7213f8c7d861632bf2e6a5c832a0c0b8a1b554e
nav797 <navneetdhillon79@gmail.com>
2024-10-30 18:24:00 -0700

    Finished Project

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ const push = document.querySelector('#push');
 const newtaskInput = document.querySelector('#newtask input');
 const tasks = document.querySelector('#tasks');
 
-push.addEventListener( 'click',function(){
+function addTask(){
     if(newtaskInput.value.length===0){
         alert("Please Enter a Task");
     }
     else{
-        tasks.innerHTML += `
-        <div class="task">
-        <span id="taskname">
+
+        const task = document.createElement('div');
+        task.className = 'task';
+        task.innerHTML = `<span id="taskname">
             ${newtaskInput.value}
         </span>
         <button class="delete">
             <i class="far fa-trash-alt"></i>
-        </button>
-        </div>
-        `;
+        </button>`
 
-        newtaskInput.value = "";
-        
+        tasks.appendChild(task);
+
+
+        newtaskInput.value = "";  
     }
-});
+}
+
+push.addEventListener( 'click', addTask);
+
+newtaskInput.addEventListener('keypress',(event)=>{
+    if(event.key==='Enter'){
+        addTask();
+    }
+})
 
 tasks.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete')) {
-        const taskToRemove = event.target.parentNode;
+    
+    const deleteButton = event.target.closest('.delete');
+    const checked = event.target.closest('.task');
+
+    if (deleteButton) {
+        const taskToRemove = deleteButton.parentNode; // Parent is the task div
         taskToRemove.remove();
+    }
+
+    if(checked){
+        checked.classList.toggle('completed');
     }
 });
 
